f37b5b3309e6927b5a5378bdf79248b7407e6292
zoghal <saleh.souzanchi@gmail.com>
2013-04-25 13:40:15 +0430

    up

--- Event/CakeGridEvent.php
+++ Event/CakeGridEvent.php
@@ -1,16 +1,11 @@
 <?php
 class CakeGridEvent extends RitaBaseEvent  {
 	
-//	public function implementedEvents(){
-//		return array(
-//			'Rita.getPageTypes' => array('callable' => 'getPageTypes' ,'priority' => 1 )	
-//		);
-//	}
-//	
 	
   	public function beforeInitController(CakeEvent $event) {
-
-		 $event->subject()->helpers[] = 'CakeGrid.Grid';
+		if(Rita::isStation()) {
+			$event->subject()->helpers[] = 'CakeGrid.Grid';
+		}
     }
 
 
