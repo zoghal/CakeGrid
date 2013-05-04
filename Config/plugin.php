747bbde832455fb7a174570a685d79a3468e382d
zoghal <saleh.souzanchi@gmail.com>
2013-05-04 07:59:47 +0430

    q

---
+++ Config/plugin.php
@@ -0,0 +1,16 @@
+<?php
+return array(
+	'title' 		=> __d('cake_grid','CakeGrid'),
+	'type' 			=> 'Component',
+	'core' 			=> true,
+	
+	'description'	=> 'Rita SMF System',
+
+	'author'		=> 'Mohammad Saleh Souzanchi',
+	'authorEmail'	=> 'saleh.souzanchi@gmail.com',
+	'authorUrl'		=> 'http://soozanchi.ir',
+	
+	'Events' 		=> array('CakeGrid'),
+	'Blocks' 		=> array()
+	
+);
