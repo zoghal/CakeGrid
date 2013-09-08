84134a33698231ee1da2ae820952267627c9bb04
zoghal <saleh.souzanchi@gmail.com>
2013-09-08 05:18:09 +0430

    update event

--- Event/CakeGridEvent.php
+++ Event/CakeGridEvent.php
@@ -1,7 +1,26 @@
 <?php
-class CakeGridEvent extends RitaBaseEvent  {
+class CakeGridEvent extends Object implements CakeEventListener {
+
+
+/**
+ * CakeGridEvent::implementedEvents()
+ * 
+ * @return
+ */
+	public function implementedEvents() {
+		return array(
+			'Controller.beforeConstruct'		=> array( 'callable' => 'beforeInitController' ),
+		);
+	}	
 	
 	
+	
+/**
+ * CakeGridEvent::beforeInitController()
+ * 
+ * @param mixed $event
+ * @return void
+ */
   	public function beforeInitController(CakeEvent $event) {
 		if(!Rita::isStation('front')) {
 			$event->subject()->helpers[] = 'CakeGrid.Grid';
